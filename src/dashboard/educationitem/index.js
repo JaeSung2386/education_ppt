9e3ee4417fbb32001923ccb06d4d1088a6488885
강재성 <kangjaeseong@gangjaeseong-ui-MacBookPro.local>
2023-07-21 20:30:13 +0900

    fontawesome Library install

--- src/dashboard/educationitem/index.js
+++ src/dashboard/educationitem/index.js
@@ -1,11 +1,13 @@
 import "./style.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function EducationItem() {
   return (
     <div className="box">
       <div className="education-body">
         <div className="education">
-          <h1>hello</h1>
+          <FontAwesomeIcon icon="fa-solid fa-angles-down" />
           <h1>hello</h1>
           <h1>hello</h1>
         </div>
