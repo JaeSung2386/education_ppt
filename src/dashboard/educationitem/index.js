8e487b5e0b6bd332738ef49dd33ccb8453b1065c
강재성 <wotjd2386@gmail.com>
2023-07-22 15:16:24 +0900

    sub part css init

--- src/dashboard/educationitem/index.js
+++ src/dashboard/educationitem/index.js
@@ -1,5 +1,5 @@
 import "./style.css";
-
+import { useState, useEffect, Fragment } from "react";
 import {
   faAnglesUp,
   faAnglesDown,
@@ -10,14 +10,31 @@ import { Link } from "react-router-dom";
 
 
 function EducationItem() {
+  const [isFold, setIsFold] = useState(false);
+
+  useEffect(() => {
+    console.log(isFold)
+  }, [isFold]);
+
+  const fontAwesomeIconClick = () => {
+    setIsFold((prev) => !prev);
+  }
+
   return (
     <div className="box">
-      <div className="education-body">
+      <div className="education-body"
+
+        style={{
+          borderBottom: "3px solid #333"
+        }}>
         <div className="education">
-          <div className="icon-box">
-            <FontAwesomeIcon icon={faAnglesDown} className="font-awesome-icon" />
+          <div className="icon-box"
+            onClick={fontAwesomeIconClick}>
+            <FontAwesomeIcon
+              icon={faAnglesDown}
+              className="font-awesome-icon" />
           </div>
-          <h1>hello</h1>
+          <h1>Rednerging</h1>
           <div className="icon-box">
             <Link>
               <FontAwesomeIcon icon={faAnglesRight} className="font-awesome-icon" />
@@ -25,6 +42,37 @@ function EducationItem() {
           </div>
         </div>
       </div>
+      {isFold && (
+        <div className="education-body" style={{ padding: "0" }}>
+          <div className="education" style={{ borderBottom: "2px solid rgb(51, 51, 51)" }}>
+            <span style={{ width: "60px" }} />
+            <h2>Web Browser Engine</h2>
+            <div className="icon-box">
+              <Link>
+                <FontAwesomeIcon icon={faAnglesRight} className="font-awesome-icon" />
+              </Link>
+            </div>
+          </div>
+          <div className="education" style={{ borderBottom: "2px solid rgb(51, 51, 51)" }}>
+            <span style={{ width: "60px" }} />
+            <h2>Web Browser Rednerging</h2>
+            <div className="icon-box">
+              <Link>
+                <FontAwesomeIcon icon={faAnglesRight} className="font-awesome-icon" />
+              </Link>
+            </div>
+          </div>
+          <div className="education" >
+            <span style={{ width: "60px" }} />
+            <h2>CSR, SSR, SSG</h2>
+            <div className="icon-box">
+              <Link>
+                <FontAwesomeIcon icon={faAnglesRight} className="font-awesome-icon" />
+              </Link>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
