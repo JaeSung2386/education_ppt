8ef7ed24227705d0a2047020d31d0de81391a7e9
강재성 <wotjd2386@gmail.com>
2023-07-22 13:45:59 +0900

    dashboard center 정렬

--- src/dashboard/educationitem/index.js
+++ src/dashboard/educationitem/index.js
@@ -1,20 +1,28 @@
 import "./style.css";
 
 import {
-  faArrowUpRightFromSquare,
-  faAngleUp,
+  faAnglesUp,
   faAnglesDown,
+  faAnglesRight
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+
 
 function EducationItem() {
   return (
     <div className="box">
       <div className="education-body">
         <div className="education">
-          <FontAwesomeIcon icon={faAnglesDown} />
+          <div className="icon-box">
+            <FontAwesomeIcon icon={faAnglesDown} className="font-awesome-icon" />
+          </div>
           <h1>hello</h1>
-          <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+          <div className="icon-box">
+            <Link>
+              <FontAwesomeIcon icon={faAnglesRight} className="font-awesome-icon" />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef } from "react";
-import { Link } from "react-router-dom";
 import { presentations, subPresentations } from "../data/presentation";
 import "./style.css";
 import EducationItem from "./educationitem";
